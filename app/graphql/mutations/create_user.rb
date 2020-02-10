893059935cde778970f0ceaaae52769474a82bd0
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-02-10 17:23:12 +0300

    WIP

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,22 @@
+module Mutations
+  class CreateUser < BaseMutation
+    argument :email, String, required: true
+    argument :first_name, String, required: true
+    argument :last_name, String, required: true
+    argument :password, String, required: true
+
+    field :id, ID, null: false
+    field :email, String, null: false
+
+    type Types::UserType
+
+    def resolve(email, first_name, last_name, password)
+      User.create(
+        email: email,
+        first_name: first_name,
+        last_name: last_name,
+        password: password
+      )
+    end
+  end
+end
