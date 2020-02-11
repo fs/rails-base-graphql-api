da047428760dff45b71632400698f3ecf668f67a
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-02-11 16:11:50 +0300

    Comments

--- db/migrate/20200210115945_create_users.rb
+++ db/migrate/20200210115945_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
+    enable_extension("citext")
+
     create_table :users do |t|
-      t.string :email
+      t.citext :email
       t.string :first_name
       t.string :last_name
       t.string :password_digest
@@ -9,6 +11,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    add_index :users, :email
+    add_index :users, :email, unique: true
   end
 end
