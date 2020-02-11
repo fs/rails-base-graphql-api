52de70158c82a9ad0adb9268cd25ad3d585c5d5c
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-02-11 14:32:23 +0300

    Some specs

---
+++ spec/graphql/queries/current_user_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe Mutations::RegisterUser do
+  let(:response) { ApplicationSchema.execute(query, query_options).as_json }
+
+  let(:query_options) do
+    { context: { current_user: user } }
+  end
+
+  let(:user) { create :user }
+
+  let(:query) do
+    <<-GRAPHQL
+      query {
+        me {
+          id
+          email
+          firstName
+          lastName
+        }
+      }
+    GRAPHQL
+  end
+
+  let(:expected_response) do
+    {"data" =>
+      { "me"=>
+        { "id"=> user.id.to_s,
+          "email"=> user.email,
+          "firstName"=> user.first_name,
+          "lastName"=> user.last_name
+        }
+      }
+    }
+  end
+
+  context "with current_user provided" do
+    it "gets current_user info" do
+      expect(response).to eq expected_response
+    end
+  end
+end
