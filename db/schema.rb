0581124386c8b8dd85fbe0f69b439f874649ebc4
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-02-11 16:16:13 +0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,17 @@
 ActiveRecord::Schema.define(version: 2020_02_10_115945) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.citext "email"
     t.string "first_name"
     t.string "last_name"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
