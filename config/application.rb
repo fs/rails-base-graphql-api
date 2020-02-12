2aaf2c37d442be192caf6b1020f43d2c6d631df3
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-02-12 15:47:16 +0300

    Revert

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,5 @@ module RailsBaseGraphqlApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.active_record.schema_format = :sql
   end
 end
