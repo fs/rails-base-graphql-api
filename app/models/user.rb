fd0b2bd8befadae5a844fbd85b9f7d2c28d7ea50
= <=>
2020-05-19 17:15:59 +0300

    quality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :refresh_tokens
+  has_many :refresh_tokens, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
--- spec/factories/refresh_tokens.rb
+++ spec/factories/refresh_tokens.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :refresh_token do
     association :user
     token { "token" }
-    expires_at  { 1.year.since }
+    expires_at { 1.year.since }
   end
 end
