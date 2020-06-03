bdfcde4da489b976cae94fc9396bcf6a26d639e6
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-06-03 11:13:37 +0300

    Edit profile information (#33)
    * Interactor to update user
    
    * Add spec for mutation
    
    * Create mutation
    
    * Expose mutation
    
    * Spec for interactor
    
    * update email & passwd
    
    * permit passwd and email args
    
    * update specs for interactor and graphql
    
    * rename parameters: newPassword->password, oldPassword->currentPassword
    
    * update interactor to use form object
    
    * spec for validator
    
    * address @ArthurZaharov's suggestions

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :signup, mutation: Mutations::SignUp
     field :signin, mutation: Mutations::SignIn
+    field :update_user, mutation: Mutations::UpdateUser
   end
 end
---
+++ spec/graphql/mutations/update_user_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+describe Mutations::UpdateUser do
+  let(:response) { ApplicationSchema.execute(query, execution_context).as_json }
+  let(:execution_context) { { context: { current_user: user } } }
+  let(:user) { create :user, password: "123456" }
+  let(:query) do
+    <<-GRAPHQL
+      mutation {
+        updateUser (
+          email: "new_email_11@example.com",
+          firstName: "Randle",
+          lastName: "McMurphy",
+          currentPassword: "123456",
+          password: "qwerty"
+        ) {
+          me {
+            id
+            email
+            firstName
+            lastName
+          }
+        }
+      }
+    GRAPHQL
+  end
+
+  let(:expected_response) do
+    {
+      "data" => {
+        "updateUser" => {
+          "me" => {
+            "id" => user.id.to_s,
+            "email" => "new_email_11@example.com",
+            "firstName" => "Randle",
+            "lastName" => "McMurphy"
+          }
+        }
+      }
+    }
+  end
+
+  it "returns updated user info" do
+    expect(response).to eq(expected_response)
+  end
+
+  it "updates user" do
+    response
+
+    expect(user).to have_attributes(
+      email: "new_email_11@example.com",
+      first_name: "Randle",
+      last_name: "McMurphy"
+    )
+  end
+end
