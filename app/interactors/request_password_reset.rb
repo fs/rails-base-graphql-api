8b2a82c50e03718fb4e35b2e1aff686eeabbe619
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-06-09 12:02:52 +0300

    use one interactor

--- app/interactors/request_password_reset.rb
+++ app/interactors/request_password_reset.rb
@@ -1,6 +1,29 @@
 class RequestPasswordReset
-  include Interactor::Organizer
+  include Interactor
+  include TransactionalInteractor
 
-  organize GenerateResetToken,
-           SendRecoveryInstructions
+  delegate :email, to: :context
+
+  def call
+    user.regenerate_password_reset_token
+    user.password_reset_sent_at = Time.current
+    user.save!
+  end
+
+  after do
+    ApplicationMailer.password_recovery(user).deliver_later
+  end
+
+  private
+
+  def user
+    context.user ||= User.find_by(email: email) || context.fail!(error_data: error_data)
+  end
+
+  def error_data
+    {
+      message: I18n.t("password_recovery.not_found.message"),
+      detail: I18n.t("password_recovery.not_found.detail", email: email)
+    }
+  end
 end
