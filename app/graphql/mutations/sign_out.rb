7307754d278db3c012492679e7733496e45b0662
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-10 13:54:21 +0300

    apply review comments 2

--- app/graphql/mutations/sign_out.rb
+++ app/graphql/mutations/sign_out.rb
@@ -2,13 +2,14 @@ module Mutations
   class SignOut < BaseMutation
     argument :everywhere, Boolean, required: false
 
-    type Types::UserType
-
+    type Types::MessageType
     def resolve(everywhere: nil)
       delete_token = SignoutUser.call(token: token, user: current_user, everywhere: everywhere)
 
       if delete_token.success?
-        current_user
+        {
+          message: "User signed out successfully"
+        }
       else
         execution_error(error_data: delete_token.error_data)
       end
--- spec/graphql/mutations/sign_out_spec.rb
+++ spec/graphql/mutations/sign_out_spec.rb
@@ -5,33 +5,37 @@ describe Mutations::SignOut, type: :request do
 
   let(:user) { create :user, id: 111_111 }
   let(:everywhere) { true }
+  let(:refresh_token) { create :refresh_token, :access, user: user }
   let(:query) do
     <<-GRAPHQL
     mutation {
       signout(everywhere: #{everywhere})
       {
-        id
+        message
       }
     }
     GRAPHQL
   end
 
   before do
+    create :refresh_token, user: user
     post "/graphql", headers: { authorization: refresh_token.token }, params: { query: query }
   end
 
   context "with valid token" do
-    let(:refresh_token) { create :refresh_token, :access, user: user }
+    it "destroy all tokens" do
+      expect(RefreshToken.count).to eq(0)
+    end
 
     it_behaves_like "full graphql request", "return current user" do
       let(:fixture_path) { "json/acceptance/signout/current_user.json" }
     end
 
-    context "when everywhere is false" do
+    context "with everywhere is false" do
       let(:everywhere) { false }
 
-      it_behaves_like "full graphql request", "return current user" do
-        let(:fixture_path) { "json/acceptance/signout/current_user.json" }
+      it "destroy one token" do
+        expect(RefreshToken.count).to eq(1)
       end
     end
   end
