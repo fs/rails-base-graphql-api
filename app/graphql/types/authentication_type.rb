963d44dbf11ff8d34a7eb05036377be436533ab5
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-15 15:41:12 +0300

    add null tokens

--- app/graphql/types/authentication_type.rb
+++ app/graphql/types/authentication_type.rb
@@ -1,7 +1,15 @@
 module Types
   class AuthenticationType < Types::BaseObject
-    field :access_token, String, null: false
-    field :refresh_token, String, null: false
+    field :access_token, String, null: true
+    field :refresh_token, String, null: true
     field :me, Types::UserType, null: true, method: :user
+
+    def access_token
+      object.access_token || nil
+    end
+
+    def refresh_token
+      object.refresh_token || nil
+    end
   end
 end
