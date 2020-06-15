27ba580c92940b07c4e1ca211679ed5d263a4cd7
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-06-15 15:12:08 +0300

    use graphql enum type for event field

--- app/graphql/types/activity_type.rb
+++ app/graphql/types/activity_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :body, String, null: false
-    field :event, String, null: false
+    field :event, ActivityEventType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
