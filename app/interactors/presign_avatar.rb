25be00e36fe42418e13cf4c4748c91de4558fd6a
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-06-16 12:08:39 +0300

    fail interactor in case type is not supported

--- app/interactors/presign_avatar.rb
+++ app/interactors/presign_avatar.rb
@@ -1,39 +1,47 @@
 class PresignAvatar
   include Interactor
 
+  ALLOWED_TYPES = %w[image/jpeg image/png image/webp].freeze
+  UPLOAD_SIZE_LIMIT = 10.megabyte
+
   delegate :filename, :type, to: :context
 
   def call
+    context.fail!(error_data: error_data) unless ALLOWED_TYPES.include?(type)
     context.presign_data = presign_data
   end
 
   private
 
   def presign_data
-    { fields: {}, headers: {} }.merge(storage.presign(location, **options).to_h)
+    { fields: {}, headers: {} }.merge(upload_params)
+  end
+
+  def upload_params
+    storage.presign(location, **options).to_h
   end
 
   def location
-    uploader.send(:generate_uid, nil) + extension
-  end
-
-  def extension
-    File.extname(filename)
+    uploader.send(:generate_uid, nil) + File.extname(filename)
   end
 
   def uploader
     Shrine.new(:cache)
   end
 
-  def storage
-    @storage ||= Shrine.find_storage(:cache)
-  end
-
   def options
     {
-      content_disposition:    ContentDisposition.inline(filename), # set download filename
-      content_type:           type,                                # set content type (defaults to "application/octet-stream")
-      content_length_range:   0..(10*1024*1024),                   # limit upload size to 10 MB
+      content_disposition:    ContentDisposition.inline(filename),
+      content_type:           type,
+      content_length_range:   0..UPLOAD_SIZE_LIMIT
     }
+  end
+
+  def storage
+    Shrine.find_storage(:cache)
+  end
+
+  def error_data
+    { message: "Wrong file type", status: 415, code: :unsupported_media_type }
   end
 end
