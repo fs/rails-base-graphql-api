e883fe2c2a7f2d27fcace04a25ea20680fe60e1e
Evgeniy Esaulkov <58729845+EvgeniyEsaulkov@users.noreply.github.com>
2020-06-16 18:14:40 +0300

    add reference to user for activity (#55)

--- spec/graphql/types/query_type_activity_spec.rb
+++ spec/graphql/types/query_type_activity_spec.rb
@@ -3,8 +3,10 @@ require "rails_helper"
 describe Types::QueryType do
   include_context "when time is frozen"
 
+  let(:user) { create(:user, email: "john.doe@email.me", first_name: "John", last_name: "Doe") }
   let!(:activity) do
     create(:activity,
+           user: user,
            title: "User registered",
            body: "New user registered with the next attributes: First Name - John, Last Name - Doe")
   end
@@ -16,6 +18,12 @@ describe Types::QueryType do
           title
           body
           createdAt
+          user {
+            id
+            email
+            firstName
+            lastName
+          }
         }
       }
     GRAPHQL
@@ -23,6 +31,6 @@ describe Types::QueryType do
 
   it_behaves_like "graphql request", "gets activities list" do
     let(:fixture_path) { "json/acceptance/graphql/query_type_activities.json" }
-    let(:prepared_fixture_file) { fixture_file.gsub(/:id/, ":id" => activity.id) }
+    let(:prepared_fixture_file) { fixture_file.gsub(/:id|:user_id/, ":id" => activity.id, ":user_id" => user.id) }
   end
 end
