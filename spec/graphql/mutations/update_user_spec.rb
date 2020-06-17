9b5a0bcf422428f5d08f4d6d3000400b6d9b1689
ArthurZaharov <arthur.zaharov@flatstack.com>
2020-06-18 01:01:04 +0300

    Update spec definition

--- spec/graphql/mutations/update_user_spec.rb
+++ spec/graphql/mutations/update_user_spec.rb
@@ -22,35 +22,12 @@ describe Mutations::UpdateUser do
     GRAPHQL
   end
 
-  let(:expected_response) do
-    {
-      "data" => {
-        "updateUser" => {
-          "id" => user.id.to_s,
-          "email" => "new_email_11@example.com",
-          "firstName" => "Randle",
-          "lastName" => "McMurphy"
-        }
-      }
-    }
-  end
-
   it_behaves_like "graphql request", "returns updated user info" do
     let(:fixture_path) { "json/acceptance/graphql/update_user.json" }
     let(:prepared_fixture_file) do
       fixture_file.gsub(
         /:id/,
         ":id" => user.id
-      )
-    end
-
-    it "updates user" do
-      response
-
-      expect(user).to have_attributes(
-        email: "new_email_11@example.com",
-        first_name: "Randle",
-        last_name: "McMurphy"
       )
     end
   end
