2fb2ea5a291770ba2d5c0ae859fbbc1276d26fc9
ArthurZaharov <arthur.zaharov@flatstack.com>
2020-06-17 20:27:06 +0300

    Use initializer instead of application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-require "sidekiq/web"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,7 +23,6 @@ module RailsBaseGraphqlApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
   mount Sidekiq::Web => "/sidekiq"
+
+  post "/graphql", to: "graphql#execute"
 end
