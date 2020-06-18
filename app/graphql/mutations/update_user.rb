7304f0a962d5c8c2e218e849db6d43feb05fc872
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-06-18 18:23:10 +0300

    add avatar to mutations

--- app/graphql/mutations/update_user.rb
+++ app/graphql/mutations/update_user.rb
@@ -5,6 +5,7 @@ module Mutations
     argument :last_name, String, required: false
     argument :current_password, String, required: false
     argument :password, String, required: false
+    argument :avatar, String, required: false
 
     type Types::AuthenticationType
 
