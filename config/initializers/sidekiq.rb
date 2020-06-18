51f47299e9d0b6e6e0dea862cf9d84eafa4c4fa4
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-06-18 14:14:01 +0300

    rename redis env variable

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,9 @@ Rails.application.configure do
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL_SIDEKIQ", "redis://localhost:6379/1") }
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL_SIDEKIQ", "redis://localhost:6379/1") }
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
 end
