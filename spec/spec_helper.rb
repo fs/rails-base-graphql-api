1bce1e104b52904854c6921a2214efc183da8844
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-06-19 19:22:02 +0300

    improve quality

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,3 @@ require "active_support/testing/time_helpers"
 RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
 end
-
-require "email_spec"
-require "email_spec/rspec"
--- spec/validators/active_reset_token_validator_spec.rb
+++ spec/validators/active_reset_token_validator_spec.rb
@@ -1,25 +1,20 @@
 require "rails_helper"
 
 describe ActiveResetTokenValidator do
-  include ActiveSupport::Testing::TimeHelpers
+  include_context "when time is frozen"
 
   let(:form) { UpdateUserPasswordForm.new(user) }
   let(:user) { build(:user, :with_reset_token) }
   let(:expiration_time) { user.password_reset_sent_at + 15.minutes }
 
   before do
-    travel_to update_time
-    freeze_time
-
     form.assign_attributes({ password: "qwerty" })
     form.validate
   end
 
-  after { travel_back }
-
   describe "active reset token validation" do
     context "when reset token is invalid" do
-      let(:update_time) { expiration_time + 1 }
+      let(:current_time) { expiration_time + 1 }
 
       it "fails and adds error" do
         expect(form.errors[:password_reset_token]).to include("has expired")
@@ -27,7 +22,7 @@ describe ActiveResetTokenValidator do
     end
 
     context "when reset token is valid" do
-      let(:update_time) { expiration_time }
+      let(:current_time) { expiration_time }
 
       it "succeeds" do
         expect(form.errors).to be_empty
