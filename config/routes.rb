553e91c148f9b176e0dff0e93664b3497467b2e9
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-22 11:52:13 +0300

    add base auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => "/sidekiq"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 
   post "/graphql", to: "graphql#execute"
 end
