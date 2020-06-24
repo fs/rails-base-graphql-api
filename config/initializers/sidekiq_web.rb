daae7c5456b86a14675e903f561cfaea3c93e8ad
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-24 14:17:25 +0300

    fix quality

--- config/initializers/sidekiq_web.rb
+++ config/initializers/sidekiq_web.rb
@@ -1,4 +1,12 @@
-Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
-end if ENV["SIDEKIQ_USERNAME"] && ENV["SIDEKIQ_PASSWORD"]
+if ENV["SIDEKIQ_USERNAME"] && ENV["SIDEKIQ_PASSWORD"]
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(
+      ::Digest::SHA256.hexdigest(username),
+      ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])
+    ) &
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(password),
+        ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"])
+      )
+  end
+end
