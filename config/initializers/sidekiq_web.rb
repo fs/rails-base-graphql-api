527a01d549e7598d02c943d6c7452476d9f47b2c
ArthurZaharov <arthur.zaharov@flatstack.com>
2020-06-24 17:25:39 +0300

    ActiveSupport encrypts input itself

--- config/initializers/sidekiq_web.rb
+++ config/initializers/sidekiq_web.rb
@@ -1,12 +1,6 @@
 if ENV["SIDEKIQ_USERNAME"] && ENV["SIDEKIQ_PASSWORD"]
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    ActiveSupport::SecurityUtils.secure_compare(
-      ::Digest::SHA256.hexdigest(username),
-      ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])
-    ) &
-      ActiveSupport::SecurityUtils.secure_compare(
-        ::Digest::SHA256.hexdigest(password),
-        ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"])
-      )
+    ActiveSupport::SecurityUtils.secure_compare(username, ENV["SIDEKIQ_USERNAME"]) &
+      ActiveSupport::SecurityUtils.secure_compare(password, ENV["SIDEKIQ_PASSWORD"])
   end
 end
