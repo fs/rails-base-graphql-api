e5eb547d97d900c722020767f6c042f949573392
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-25 17:19:21 +0300

    change code

--- app/graphql/types/activity_event_type.rb
+++ app/graphql/types/activity_event_type.rb
@@ -2,6 +2,6 @@ module Types
   class ActivityEventType < Types::BaseEnum
     value "USER LOGGED IN", value: "user_logged_in"
     value "USER REGISTERED", value: "user_registered"
-    value "Reset Password Request", value: "reset_password_request"
+    value "RESET PASSWORD REQUEST", value: "reset_password_request"
   end
 end
