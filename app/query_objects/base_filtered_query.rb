ab6c651330103e7eb88ba560baf1c01e40ef38dc
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-29 12:19:00 +0300

    add base filtered query

---
+++ app/query_objects/base_filtered_query.rb
@@ -0,0 +1,24 @@
+class BaseFilteredQuery
+  ALLOWED_PARAMS = [].freeze
+
+  def initialize(relation, filter_params = {})
+    @relation = relation
+    @filter_params = sanitized_filter_params(filter_params)
+  end
+
+  def all
+    filter_params.reduce(relation) do |relation, (key, value)|
+      method_name = "by_#{key}"
+      self.class.private_method_defined?(method_name) ? send(method_name, relation, value) : relation
+    end
+  end
+
+  private
+
+  def sanitized_filter_params(params)
+    params
+      .to_h
+      .deep_symbolize_keys
+      .reject { |k, v| !self.class::ALLOWED_PARAMS.include?(k) || v.to_s.blank? }
+  end
+end
