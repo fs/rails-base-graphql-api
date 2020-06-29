7f8507cbe4287174a65e2623cc05bf8d33f692e0
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-06-29 17:36:24 +0300

    fix code

--- app/query_objects/base_filtered_query.rb
+++ app/query_objects/base_filtered_query.rb
@@ -1,24 +1,16 @@
 class BaseFilteredQuery
-  ALLOWED_PARAMS = [].freeze
-
   def initialize(relation, filter_params = {})
     @relation = relation
-    @filter_params = sanitized_filter_params(filter_params)
+    @filter_params = filter_params
   end
 
   def all
     filter_params.reduce(relation) do |relation, (key, value)|
-      method_name = "by_#{key}"
-      self.class.private_method_defined?(method_name) ? send(method_name, relation, value) : relation
+      send("by_#{key}", relation, value)
     end
   end
 
   private
 
-  def sanitized_filter_params(params)
-    params
-      .to_h
-      .deep_symbolize_keys
-      .reject { |k, v| !self.class::ALLOWED_PARAMS.include?(k) || v.to_s.blank? }
-  end
+  attr_reader :relation, :filter_params
 end
