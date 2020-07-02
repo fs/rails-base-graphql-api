78624f5472b5d4e2ec7cc6625de292cefbe17183
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-07-02 11:07:11 +0300

    restore BaseFilteredQuery

--- app/query_objects/base_filtered_query.rb
+++ app/query_objects/base_filtered_query.rb
@@ -1,23 +1,16 @@
 class BaseFilteredQuery
-  def initialize(relation, filter_params = {}, pagination_params = {})
+  def initialize(relation, filter_params = {})
     @relation = relation
     @filter_params = filter_params
-    @pagination_params = pagination_params
   end
 
   def all
-    filtered_relation
-      .offset(pagination_params[:offset])
-      .limit(pagination_params[:limit])
-  end
-
-  private
-
-  attr_reader :filter_params, :pagination_params, :relation
-
-  def filtered_relation
     filter_params.reduce(relation) do |relation, (key, value)|
       public_send("by_#{key}", relation, value)
     end
   end
+
+  private
+
+  attr_reader :relation, :filter_params
 end
