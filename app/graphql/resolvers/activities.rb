d3033788328f1fdc3c8b91698b0a92ac42177031
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-07-16 12:45:44 +0300

    Add filter to activities by event (#89)

--- app/graphql/resolvers/activities.rb
+++ app/graphql/resolvers/activities.rb
@@ -1,9 +1,15 @@
 module Resolvers
   class Activities < Resolvers::Base
+    argument :events, [Types::ActivityEventType], required: false
+
     type [Types::ActivityType], null: true
 
-    def resolve
-      Activity.all
+    def fetch_data
+      FilteredActivitiesQuery.new(raw_relation, options).all.to_a
+    end
+
+    def raw_relation
+      object ? Activity.where(user_id: object.id) : Activity.all
     end
   end
 end
--- app/graphql/resolvers/base.rb
+++ app/graphql/resolvers/base.rb
@@ -2,7 +2,18 @@ module Resolvers
   class Base < GraphQL::Schema::Resolver
     argument_class Types::BaseArgument
 
+    def resolve(**options)
+      @options = options
+      fetch_data
+    end
+
     private
+
+    attr_reader :options
+
+    def fetch_data
+      raise NotImplementedError
+    end
 
     def current_user
       @current_user ||= context[:current_user]
