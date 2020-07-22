ebf8f64a3d682e28ffb3a5b5cb78ffa829c206a3
AzatZaynutdinov <azat.zaynutdinov@flatstack.com>
2020-07-22 10:13:35 +0300

    escaping

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, "query"]
+Rails.application.config.filter_parameters += [:password]
+
+Rails.application.config.filter_parameters << lambda do |param_name, value|
+  if %w[query].include?(param_name) && value.respond_to?(:gsub!)
+    value.gsub!(/[\r\n\"]+/, "")
+  end
+end
