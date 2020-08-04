5c75beec924f30e28203d94e861f5efe14258932
Arthur Zaharov <arthur.zaharov@flatstack.com>
2020-08-04 14:55:10 +0300

    Fix issues with events and nil avatar (#109)
    * Update activities events list
    
    * Use delegate for avatar url

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,8 @@ class Activity < ApplicationRecord
 
   belongs_to :user
 
-  enumerize :event, in: %i[user_registered user_updated reset_password_requested user_reset_password]
+  enumerize :event, in: %i[user_registered user_logged_in user_updated reset_password_requested
+                           user_reset_password]
 
   validates :title, :body, presence: true
 end
