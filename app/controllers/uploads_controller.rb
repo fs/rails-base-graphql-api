30d233d5be7d618c9c238053ca3723dcd5476a89
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-10-05 19:22:05 +0300

    add uploads controller

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,13 @@
+class UploadsController < ApplicationController
+  def image
+    set_rack_response ImageUploader.upload_response(:cache, request.env)
+  end
+
+  private
+
+  def set_rack_response(status, headers, body)
+    self.status = status
+    self.headers.merge!(headers)
+    self.response_body = body
+  end
+end
