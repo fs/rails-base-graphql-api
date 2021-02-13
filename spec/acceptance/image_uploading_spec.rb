b273195c5f2bca972d8132ec10ea523c46f55999
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2021-02-13 11:27:42 +0300

    add test for image uploading

---
+++ spec/acceptance/image_uploading_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "Upload image", type: :request do
+  let(:avatar_image_path) { Rails.root.join("spec/fixtures/images/avatar.jpg") }
+  let(:file_key) { "c379cbc5050227055116049764dc283b.png" }
+  let(:params) do
+    {
+      key: file_key,
+      content_disposition: "inline; filename='avatar.png'; filename*=UTF-8''avatar.png",
+      content_type: "image/png",
+      content_length_range: "0..10485760",
+      file: fixture_file_upload(avatar_image_path, 'image/png')
+    }
+  end
+
+  before { post "/images/upload", params: params }
+  after { Shrine.storages[:cache].clear! }
+
+  it "uploads image" do
+    expect(Shrine.storages[:cache].exists?(file_key)).to be_truthy
+  end
+
+  it "returns 204 status" do
+    expect(response.status).to eq(204)
+    expect(response.body).to be_empty
+  end
+end
