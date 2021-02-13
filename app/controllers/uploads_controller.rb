64eee4eaaad5d6e1089c9be6e22a5ba2d4aaea7a
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2021-02-13 10:44:21 +0300

    simplify uploads controller

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,13 +1,5 @@
 class UploadsController < ApplicationController
   def image
-    set_rack_response ImageUploader.upload_response(:cache, request.env)
-  end
-
-  private
-
-  def set_rack_response((status, headers, body))
-    self.status = status
-    self.headers.merge!(headers)
-    self.response_body = body
+    ImageUploader.upload_response(:cache, request.env)
   end
 end
