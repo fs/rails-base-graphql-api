4963f33f33b8ee71b7938811c250e5f561489f12
EvgeniyEsaulkov <evgeniy.esaulkov@flatstack.com>
2021-10-28 09:52:24 +0300

    improve quality

--- spec/graphql/mutations/request_password_recovery_spec.rb
+++ spec/graphql/mutations/request_password_recovery_spec.rb
@@ -16,16 +16,15 @@ describe Mutations::RequestPasswordRecovery do
     GRAPHQL
   end
 
-
   context "when user exists" do
-    let!(:user) { create :user, email: "zaphod.beeblebrox@gmail.com" }
     let(:fixture_path) { "json/acceptance/graphql/request_password_recovery.json" }
+
+    before { create :user, email: "zaphod.beeblebrox@gmail.com" }
 
     it_behaves_like "graphql request", "returns info message"
   end
 
   context "when user doesn't exist" do
-    let!(:user) { build :user }
     let(:fixture_path) { "json/acceptance/graphql/request_password_recovery_wrong.json" }
 
     it_behaves_like "graphql request", "returns error"
