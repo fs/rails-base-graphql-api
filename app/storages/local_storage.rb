127d4180ad3407b4d9a57e36fec01760d30bbabd
Evgeniy Esaulkov <58729845+EvgeniyEsaulkov@users.noreply.github.com>
2021-10-29 14:32:49 +0300

    Convert field names for local storage (#184)
    * convert field names for local storage
    
    * improve quality

--- app/storages/local_storage.rb
+++ app/storages/local_storage.rb
@@ -1,11 +1,19 @@
 class LocalStorage < Shrine::Storage::FileSystem
+  CONVERSION_FIELDS = {
+    cache_control: "Cache-Control",
+    content_disposition: "Content-Disposition",
+    content_encoding: "Content-Encoding",
+    content_length_range: "content-length-range",
+    content_type: "Content-Type"
+  }.freeze
+
   delegate :images_upload_url, to: :url_helpers
 
   def presign(id, **options)
     {
       method: :post,
       url: images_upload_url(default_url_options),
-      fields: { key: [storage_key, id].join("/") }.merge(options)
+      fields: { key: [storage_key, id].join("/") }.merge(converted_options(options))
     }
   end
 
@@ -27,5 +35,13 @@ class LocalStorage < Shrine::Storage::FileSystem
 
   def default_url_options
     Rails.application.config.action_controller.default_url_options
+  end
+
+  def converted_options(options)
+    CONVERSION_FIELDS.each do |key, value|
+      options[value] = options.delete(key)
+    end
+
+    options.compact
   end
 end
