81c28c4c769ee0749de459bb42a9d66ac428c6f8
Arthur Zaharov <arthur.zaharov@flatstack.com>
2021-10-29 19:33:57 +0300

    Add n plus one control gem (#183)
    * Add n plus one control gem
    
    * n+1 actiities
    
    * remove let variable
    
    * improve quality
    
    * add shared examples
    
    * Fix quality
    
    Co-authored-by: EvgeniyEsaulkov <evgeniy.esaulkov@flatstack.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require "active_support/testing/time_helpers"
+require "n_plus_one_control/rspec"
 
 RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
