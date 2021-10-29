9c78a64e7329ef032e243086fa6fefcb1cf82638
EvgeniyEsaulkov <evgeniy.esaulkov@flatstack.com>
2021-10-29 17:02:50 +0300

    remove let variable

--- spec/graphql/types/query_type_activities_all_spec.rb
+++ spec/graphql/types/query_type_activities_all_spec.rb
@@ -3,8 +3,6 @@ require "rails_helper"
 describe Types::QueryType, :n_plus_one do
   let!(:user) { create :user }
 
-  let(:response) { ApplicationSchema.execute(query, {}).as_json }
-
   let(:query) do
     <<-GRAPHQL
       query {
@@ -26,6 +24,6 @@ describe Types::QueryType, :n_plus_one do
   populate { |n| create_list :activity, n, user: user }
 
   specify do
-    expect { response }.to perform_constant_number_of_queries
+    expect { ApplicationSchema.execute(query, {}).as_json }.to perform_constant_number_of_queries
   end
 end
