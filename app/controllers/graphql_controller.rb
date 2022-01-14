1909a85098972442ba63f222c2c0dae51f49399f
Sergey Antonov <sergey.antonov@flatstack.com>
2022-01-14 16:32:01 +0300

    Add ability to trigger events in GraphQL resolvers

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,5 +1,7 @@
 class GraphqlController < ApplicationController
   def execute
+    trigger_events if execution_success?
+
     render json: execute_query
   rescue StandardError => e
     raise e unless Rails.env.development?
@@ -10,7 +12,7 @@ class GraphqlController < ApplicationController
   private
 
   def execute_query
-    ApplicationSchema.execute(
+    @execute_query ||= ApplicationSchema.execute(
       params[:query],
       variables: ensure_hash(params[:variables]),
       context: execution_context.deep_symbolize_keys,
@@ -25,6 +27,16 @@ class GraphqlController < ApplicationController
       token_payload: payload,
       extensions: ensure_hash(params[:extensions])
     }
+  end
+
+  def trigger_events
+    execute_query.context[:trigger_events].reject(&:blank?).each do |trigger_event|
+      ActiveSupport::Notifications.instrument trigger_event.event, trigger_event.options
+    end
+  end
+
+  def execution_success?
+    execute_query.to_h["errors"].blank?
   end
 
   def ensure_hash(ambiguous_param)
--- app/graphql/resolvers/base.rb
+++ app/graphql/resolvers/base.rb
@@ -1,11 +1,13 @@
 module Resolvers
   class Base < GraphQL::Schema::Resolver
     include ActionPolicy::GraphQL::Behaviour
+    include TriggerableEvents
 
     argument_class Types::BaseArgument
 
     def resolve(**options)
       @options = options
+      append_trigger_event(trigger_event) if trigger_event.present?
       fetch_data
     end
 
@@ -19,6 +21,9 @@ module Resolvers
 
     def current_user
       @current_user ||= context[:current_user]
+    end
+
+    def trigger_event
     end
   end
 end
