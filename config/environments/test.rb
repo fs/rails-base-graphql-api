c151cbc9559f6b4524cd84d22c4b89241c082fa1
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-15 13:35:25 +0300

    Turn on rail 6.1 defaults

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Set test queue adapter
+  config.active_job.queue_adapter = :test
 end
