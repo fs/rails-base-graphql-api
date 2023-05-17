c4cdbfee39512df2a454c336f9ec670c29b035c8
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-17 12:56:03 +0300

    fix quality

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,23 +3,23 @@ module Types
   class MutationType < Types::BaseObject
     description "Mutations"
 
-    field :signup, mutation: Mutations::SignUp, description: "Sign up"
-    field :signin, mutation: Mutations::SignIn, description: "Sign in"
-    field :signout, mutation: Mutations::SignOut, description: "Sign out"
+    field :sign_in, mutation: Mutations::SignIn, description: "Sign in"
+    field :sign_out, mutation: Mutations::SignOut, description: "Sign out"
+    field :sign_up, mutation: Mutations::SignUp, description: "Sign up"
 
+    field :update_password, mutation: Mutations::UpdatePassword, description: "Update user password"
     field :update_token, mutation: Mutations::UpdateToken, description: "Update short live access token"
     field :update_user, mutation: Mutations::UpdateUser, description: "Update user info"
-    field :update_password, mutation: Mutations::UpdatePassword, description: "Update user password"
 
     field :confirm_user,
           mutation: Mutations::ConfirmUser,
           description: "Confirm user email"
+    field :omniauth_sign_in_or_sign_up,
+          mutation: Mutations::OmniauthSignInOrSignUp,
+          description: "Authenticate with OAuth"
     field :request_password_recovery,
           mutation: Mutations::RequestPasswordRecovery,
           description: "Reset password request"
-    field :omniauth_signin_or_signup,
-          mutation: Mutations::OmniauthSignInOrSignUp,
-          description: "Authenticate with OAuth"
 
     field :presign_data,
           mutation: Mutations::PresignData,
