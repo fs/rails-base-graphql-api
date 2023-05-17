ff7f24b983e7b6cbcd8c9974649c81713c8fbba9
Arthur Zaharov <arthur.zaharov@flatstack.com>
2023-05-17 13:37:18 +0300

    Add session store to fix sidekiq web ui (#234)
    * Add session store to fix sidekiq web ui
    
    * use env var
    
    * fix quality

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,9 @@
+Rails.application.configure do
+  # This also configures session_options for use below
+  config.session_store :cookie_store, key: ENV.fetch("SESSION_STORE_KEY", "#{Rails.root.basename}_session")
+
+  # Required for all session management (regardless of session_store)
+  config.middleware.use ActionDispatch::Cookies
+
+  config.middleware.use config.session_store, config.session_options
+end
