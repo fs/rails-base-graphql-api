061c1706d72a902a89615f3fc71520b70545a9fa
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-17 13:27:52 +0300

    use env var

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # This also configures session_options for use below
-  config.session_store :cookie_store, key: "_rails_base_graphql_api_session"
+  config.session_store :cookie_store, key: ENV.fetch("SESSION_STORE_KEY", "#{File.basename(Rails.root)}_session")
 
   # Required for all session management (regardless of session_store)
   config.middleware.use ActionDispatch::Cookies
