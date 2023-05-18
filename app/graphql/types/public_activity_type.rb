180e02a0554c3bc861ae8779ff7f270297e80c83
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-18 12:46:43 +0300

    remove useless loader

--- app/graphql/types/public_activity_type.rb
+++ app/graphql/types/public_activity_type.rb
@@ -6,9 +6,5 @@ module Types
 
     field :body, String, "Body", null: false
     field :title, String, "Title", null: false
-
-    def user
-      Loaders::RecordLoader.for(User).load(object.user_id)
-    end
   end
 end
