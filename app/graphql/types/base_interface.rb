1596ea5f746912aa28078d5cfe0ce8188e4f09df
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-18 20:50:59 +0300

    update graphql to 2.0

--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -1,6 +1,8 @@
 module Types
   module BaseInterface
     include GraphQL::Schema::Interface
+    edge_type_class(Types::BaseEdge)
+    connection_type_class(Types::BaseConnection)
 
     field_class Types::BaseField
   end
--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,5 +1,7 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
+    edge_type_class(Types::BaseEdge)
+    connection_type_class(Types::BaseConnection)
     include ActionPolicy::GraphQL::Behaviour
 
     field_class Types::BaseField
