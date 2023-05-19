9d0f8f4f7cfbc6caaacb4856ba18b55015218b7c
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-19 19:44:26 +0300

    update rails to 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_145805) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_06_04_145805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -21,25 +20,25 @@ ActiveRecord::Schema.define(version: 2021_06_04_145805) do
     t.text "body", null: false
     t.string "event", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "possession_tokens", force: :cascade do |t|
     t.string "value", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_possession_tokens_on_user_id"
   end
 
   create_table "refresh_tokens", force: :cascade do |t|
     t.string "token", null: false
     t.bigint "user_id", null: false
-    t.datetime "expires_at", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "expires_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "jti"
     t.index ["jti"], name: "index_refresh_tokens_on_jti"
     t.index ["token"], name: "index_refresh_tokens_on_token"
@@ -51,12 +50,12 @@ ActiveRecord::Schema.define(version: 2021_06_04_145805) do
     t.string "first_name"
     t.string "last_name"
     t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "avatar_data"
     t.string "password_reset_token"
-    t.datetime "password_reset_sent_at"
-    t.datetime "confirmed_at"
+    t.datetime "password_reset_sent_at", precision: nil
+    t.datetime "confirmed_at", precision: nil
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_reset_token"], name: "index_users_on_password_reset_token"
   end
