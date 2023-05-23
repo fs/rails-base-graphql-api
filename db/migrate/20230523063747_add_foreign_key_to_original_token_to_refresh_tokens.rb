875b6c9f30f2d72baeb015c9b2ad7ea0a3285a49
ArthurZaharov <arthur.zaharov@flatstack.com>
2023-05-23 14:04:45 +0300

    validate foreign key

--- db/migrate/20230523063747_add_foreign_key_to_original_token_to_refresh_tokens.rb
+++ db/migrate/20230523063747_add_foreign_key_to_original_token_to_refresh_tokens.rb
@@ -1,5 +1,7 @@
 class AddForeignKeyToOriginalTokenToRefreshTokens < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key :refresh_tokens, :refresh_tokens, column: :original_token_id, validate: false
+    safety_assured do
+      add_foreign_key :refresh_tokens, :refresh_tokens, column: :original_token_id
+    end
   end
 end
